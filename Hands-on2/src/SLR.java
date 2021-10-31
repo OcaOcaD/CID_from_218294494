6a05e77f297c3aecbd3e1408e4a799283390fbf6
OcaOcaD <luisdonaldogarciacastro@gmail.com>
2021-10-31 13:43:42 -0600

    SLR agent 'finished'

--- Hands-on2/src/SLR.java
+++ Hands-on2/src/SLR.java
@@ -7,37 +7,36 @@ import java.util.HashMap;
 
 public class SLR extends Agent{
 
-
-    protected HashMap <Integer, Integer> s = new HashMap <Integer, Integer> (); // Sales
-    protected HashMap <Integer, Integer> a = new HashMap <Integer, Integer> (); // Advertising
-
+    protected double s[] = new double [9]; // sales
+    protected double a[] = new double [9]; // advertising
     /**
      * Data taken from:
      * url: https://www.displayr.com/what-is-linear-regression/
      */
     private void fillDataSet(){
         // Filling Sales by year
-        s.put(1, 651);
-        s.put(2, 762);
-        s.put(3, 856);
-        s.put(4, 1064);
-        s.put(5, 1190);
-        s.put(6, 1298);
-        s.put(7, 1421);
-        s.put(8, 1440);
-        s.put(9, 1518);
+        s[0] = 651;
+        s[1] = 762;
+        s[2] = 856;
+        s[3] = 1064;
+        s[4] = 1190;
+        s[5] = 1298;
+        s[6] = 1421;
+        s[7] = 1440;
+        s[8] = 1518;
         // Filling advertising by Year
-        a.put(1, 23);
-        a.put(2, 26);
-        a.put(3, 30);
-        a.put(4, 34);
-        a.put(5, 43);
-        a.put(6, 48);
-        a.put(7, 52);
-        a.put(8, 57);
-        a.put(9, 58);
+        a[0] = 23;
+        a[1] = 26;
+        a[2] = 30;
+        a[3] = 34;
+        a[4] = 43;
+        a[5] = 48;
+        a[6] = 52;
+        a[7] = 57;
+        a[8] = 58;
     }
     protected void setup() {
+
         System.out.println("Agent "+getLocalName()+" started.");
         fillDataSet();
         addBehaviour(new CyclicSLRbehaviur());
@@ -48,25 +47,47 @@ public class SLR extends Agent{
     }
     //
     private class CyclicSLRbehaviur extends CyclicBehaviour {
-        int i = 0;
-        int x;
-        int y;
+        int runs;
+        double beta0 = 0;
+        double beta1 = 0;
+        double alpha = 0.003;
+        double xi;
+        double yi;
+        // Error por iteracion
+        double error_sum = 0;   //  -2/9 SUM Error
         public void action() {
-            if( i < 1000 && i < s.size() ){
-                // Regression line: y = b0 + (b1)x + e
-                x = a.get(i+1); // advertising
-                y = s.get(i+1); // sales
-                sd// y hat ???
-                System.out.println(i + ": " + x + "," + y);
-                i++;
+            // STEP 2: Error cuadratico medio
+            // 2.1 Para calcularlo hay que iterar por nuestros datos y obtenerlo
+            double ei = 0;
+            for (int i = 0; i < s.length; i++){
+                yi = s[i];
+                xi = a[i];
+                // System.out.println(yi + xi);
+                ei = (xi)*(yi*(-1*beta0)*(-1*(beta1*(xi))));
+                // Pos ya calculalo
+                error_sum += ei;
+            }
+            error_sum = error_sum*-2;
+            error_sum = error_sum / s.length;
+
+            if( isSmallEnough( error_sum ) ){
+                System.out.println("Found the small error:" + error_sum);
+                  doDelete();
             }else{
-                doDelete();d
+                // Error not so small. Step forward
+                System.out.println(runs + ":\t" +"b0: " + beta0 + "\tb1: " + beta1 +  " \n\tError:  "+ error_sum + " ...");
+                beta0 = beta0 + ( alpha * error_sum ) + alpha;
+                beta1 = beta1 + ( alpha * error_sum ) + alpha;
+                runs++;
+            }
+            if (runs >= 200){
+                doDelete();
             }
         }
     }
+    private boolean isSmallEnough( double error ){
+        //System.out.println("\tError:  "+ error + " ...");
+        return false;
+    }
 
 }
-
-
-
-
